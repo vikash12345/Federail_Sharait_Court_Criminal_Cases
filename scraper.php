7723a3c34e1498f4ea564dd0cd181756f15666f2
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-03-01 12:04:54 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,22 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+/*This scraper for Federal Sharait Court of Criminal Cases.
+Link :  http://federalshariatcourt.gov.pk/c1.html
+Created By Vikash Harjeewan
+Date  : 3/1/2018
+I added daily schedules for scrape updated data regular
+*/
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$totalpages   = 36;
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+
+for($page = 0;$page <= $totalpages; $page++)
+	{
+		$link	=	'http://federalshariatcourt.gov.pk/c$page.html';
+		echo "$link\n";
+			
+	
+	}
+
 ?>
